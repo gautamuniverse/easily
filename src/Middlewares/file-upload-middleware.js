ffdd76371d3a554a5c01903ccfdbe1ac54fb8430
gautamuniverse <gautamthapameriid@gmail.com>
2023-11-23 18:38:26 +0530

    Update file-upload-middleware.js

--- src/Middlewares/file-upload-middleware.js
+++ src/Middlewares/file-upload-middleware.js
@@ -8,7 +8,7 @@ import path from 'path';
 //Below is the storage configuration that is accepted as a parameter by the multer instance.
 const storageConfig = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.resolve('public', 'resume')); //Here null means no errors. and second parameter is for the storage location where we want to store the client uploaded files.
+    cb(null, path.resolve(path.join('public', 'resume'))); //Here null means no errors. and second parameter is for the storage location where we want to store the client uploaded files.
   },
   filename: (req, file, cb) => {
     const fileName = Date.now() + "-" + file.originalname;
